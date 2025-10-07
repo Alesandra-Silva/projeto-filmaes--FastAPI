227fd46be8bb4200c479dc25a7f89853f246367f
Alesandra <alesandra.a.silva@aluno.senai.br>
2025-10-07 16:29:14 -0300

    fução criar tabela no banco de dados

---
+++ back-end/fucao.py
@@ -0,0 +1,22 @@
+from conexao import conectar
+
+def criar_tabela():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS filmes(
+                    id SERIAL PRIMARY KEY,
+                    titulo TEXT NOT NULL,
+                    genero TEXT NOT NULL,
+                    ano INTEGER NOT NULL,
+                    avaliacao REAL
+                    )
+            """)
+            conexao.commit()
+        except Exception as erro:
+            print(f"erro ao criar a tabela")
+        finally:
+            cursor.close()
+            conexao.close()
+criar_tabela()            
