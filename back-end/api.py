98ccf7a32bb55fc6a1b997af905485952d53bf2b
Alesandra Silva <alesandra.a.silva@aluno.senai.br>
2025-10-13 16:17:56 -0300

    rot listar filmes✔

--- back-end/api.py
+++ back-end/api.py
@@ -19,3 +19,17 @@ def home():
 def cria_filmes(título: str, genero: str, ano: int, avaliacao: float):
     funcao.inserir_filmes(título, genero, ano, avaliacao)
     return { "mensagem": "Filme adicionado com sucesso!"}
+
+@app.get("/filmes")
+def listar_filmes():
+    filmes = funcao.listar_filme()
+    lista = []
+    for linha in filmes:
+        lista.append({ 
+            "id": linha[0],
+            "título":linha[1],
+            "genero": linha[2],
+            "ano":linha[3],
+            "avaliacao": linha[4]
+            })
+    return {"filmes": lista}
